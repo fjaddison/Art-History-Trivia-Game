7478ce0c98a5d76163341a7c9005551708d82a5f
Francis Julian Addison <faddison@Franciss-MacBook.local>
2017-12-11 12:26:05 -0500

    pseudo code added, structure further along

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
     console.log('hello')
+    // I will need to target each input button with an event listener
+    // each response will work with an if/else statement
+    // if response matches (lower or uppercase), ten point added to box element containing score
+    // else no points added
+    // each event should also make button dissappear (hide), so user can't answer twice
+    // if score === 100, style change implemented (green screen or animation etc)
+    // if 90, alert(you got an A- or 90%)
+    // if 80, alert(you got a B-)
+    // and so forth until...
+    // if score <= 50 alert(you failed, try again)
+    // button to refresh if possible
 })
